411d56711c16f3e6d0b19406d4e00b7d1f05711f
Syed Taha Faisal <faisasye@shernet.sheridancollege.ca>
2023-06-19 22:10:14 -0400

    Added npm packages to make readline work.

--- RockPaperScissors/script.js
+++ RockPaperScissors/script.js
@@ -1,7 +1,12 @@
 function playRound(playerSelection, computerSelection) {
   // your code here!
 }
- 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+-
+const readlineSync = require('readline-sync');
+console.log("test")
+const playerSelection = readlineSync.question('Please select one: Rock/Paper/Scissors ');
+console.log(playerSelection)
+
+
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
