940cfec82a3cbf8e5de7b50464349ed32faa6577
Syed Taha Faisal <faisasye@shernet.sheridancollege.ca>
2023-06-29 09:41:41 -0400

    made grid dynamic, size change button working

--- EtchASketch/script.js
+++ EtchASketch/script.js
@@ -1,16 +1,42 @@
 window.onload = function() {
-    let input = 16
+    populateGrid(16)
+    console.log("Page loaded!");
+};
+
+
+const slider = document.querySelector("#myRange")
+const sliderP = document.querySelector(".controls p")
+
+slider.addEventListener("input", () => {
+    sliderP.textContent = slider.value
+})
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function populateGrid(size) {
     const container = document.querySelector("#container")
-    for (let i = 0; i < input; i++) {
+    removeAllChildNodes(container)
+    for (let i = 0; i < size; i++) {
         const row = document.createElement("div")
         row.className = "row"
-        for (let j = 0; j < input; j++) {
+        for (let j = 0; j < size; j++) {
             const square = document.createElement("div")
             square.className = "square"
             row.appendChild(square)
         }
         container.appendChild(row)
     }
-    console.log("Page loaded!");
-};
+}
+
+
+
+const sizeButton = document.querySelector("#sizeButton")
+
+sizeButton.addEventListener("click", () => {
+    populateGrid(slider.value)
+})
 
