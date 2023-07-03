a892fe82d65ab309eeb83d3c1a8774ca66b8dc9d
Syed Taha Faisal <faisasye@shernet.sheridancollege.ca>
2023-07-03 10:33:50 -0400

    Added functionality to color & rainbow button

--- EtchASketch/script.js
+++ EtchASketch/script.js
@@ -5,10 +5,28 @@ window.onload = function() {
 
 const colorPicker = document.querySelector("#colorPicker");
 var selectedColor = "rgb(0,0,0)"
+var rainbowOn = false
 
 colorPicker.addEventListener("change", (event) => {
+  rainbowOn = false
   selectedColor = event.target.value;
 });
+
+//COLOR BUTTON
+const colorButton = document.querySelector("#colorButton")
+
+colorButton.addEventListener("click", () => {
+  rainbowOn = false
+})
+
+//RAINBOW BUTTON
+
+const rainbowButton = document.querySelector("#rainbowButton")
+
+rainbowButton.addEventListener("click", () => {
+  rainbowOn = true
+})
+
 
 const slider = document.querySelector("#myRange")
 const sliderP = document.querySelector(".controls p")
@@ -54,14 +72,26 @@ let isMouseDown = false;
 container.addEventListener("mousemove", (event) => {
   const selectedSquare = event.target;
   if (selectedSquare.classList.contains("square") && isMouseDown && event.button === 0) {
-    selectedSquare.setAttribute("style", `background-color: ${selectedColor}`)
+    if (rainbowOn == true) {
+      let randomColor = `rgb(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256})`
+      selectedSquare.setAttribute("style", `background-color: ${randomColor}` )
+    }
+    else{
+      selectedSquare.setAttribute("style", `background-color: ${selectedColor}`)
+    }
   }
 });
 
 container.addEventListener("mousedown", (event) => {
   const selectedSquare = event.target;
   if (selectedSquare.classList.contains("square") && event.button === 0) {
-    selectedSquare.setAttribute("style", `background-color: ${selectedColor}`)
+    if (rainbowOn == true) {
+      let randomColor = `rgb(${Math.random() * 256},${Math.random() * 256},${Math.random() * 256})`
+      selectedSquare.setAttribute("style", `background-color: ${randomColor}` )
+    }
+    else{
+      selectedSquare.setAttribute("style", `background-color: ${selectedColor}`)
+    }
     isMouseDown = true;
   }
 });
@@ -81,4 +111,5 @@ container.addEventListener("contextmenu", (event) => {
 container.addEventListener("mouseleave", () => {
   isMouseDown = false;
 });
+
 
