6c2f4f4ff2c6a0e6775950baf9e2b7bb23fc465c
Syed Taha Faisal <faisasye@shernet.sheridancollege.ca>
2023-07-20 15:22:09 -0400

    added styling to validation

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -1,26 +1,32 @@
 function passwordMatch(password1, password2) {
     if (password1.value === password2.value) {
+      // Set border color to green when passwords match
+      password2.style.borderColor = 'green';
       return true;
     } else {
+      // Set border color to red when passwords don't match
+      password2.style.borderColor = 'red';
       return false;
     }
   }
   
-  document.querySelector("#errorMessage").style.opacity = "0"; // Hide the error message initially
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector("#errorMessage").style.opacity = "0"; // Hide the error message initially
   
-  let confirmbtn = document.querySelector('input[type="submit"]');
-  confirmbtn.addEventListener('click', (event) => {
-    event.preventDefault(); // Prevent the form from submitting initially
-    let password1 = document.querySelector("#password");
-    let password2 = document.querySelector("#password2");
-    let errorMessage = document.querySelector("#errorMessage");
+    let confirmbtn = document.querySelector('input[type="submit"]');
+    confirmbtn.addEventListener('click', (event) => {
+      event.preventDefault(); // Prevent the form from submitting initially
+      let password1 = document.querySelector("#password");
+      let password2 = document.querySelector("#password2");
+      let errorMessage = document.querySelector("#errorMessage");
   
-    if (passwordMatch(password1, password2)) {
-      // If passwords match, submit the form
-      event.target.form.submit();
-    } else {
-      // Show the error message
-      errorMessage.style.opacity = "100";
-    }
+      if (passwordMatch(password1, password2)) {
+        // If passwords match, submit the form
+        event.target.form.submit();
+      } else {
+        // Show the error message
+        errorMessage.style.opacity = "100";
+      }
+    });
   });
   
